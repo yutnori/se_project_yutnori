30f92da5eadac6874fbe3aec8860833229c5dd8e
Seong Hyun <42994421+seong0318@users.noreply.github.com>
2019-05-20 18:48:36 +0900

    Add files via upload
    클래스 다이어그램의 내용을 간략히 코드로 구현함

---
+++ seproject_yutnori/Piece.java
@@ -0,0 +1,11 @@
+package seproject;
+import java.util.*;
+
+public class Piece {
+	/* 말 */
+	LinkedList<Piece> pieceList = new LinkedList<Piece>();	// 업혀있는 말들(다시 생각해봐야 함)
+	
+	void groupePiece() {
+		/* 말을 업는 메소드 */
+	}
+}
---
+++ seproject_yutnori/Yut.java
@@ -0,0 +1,7 @@
+package seproject;
+
+public class Yut {
+	/* À·À» ³ªÅ¸³¿ */
+	int status;					// 0 ¶Ç´Â 1·Î À­¸é, µÞ¸é ±¸ºÐ
+	boolean isDotYut = false;	// trueÀÌ¸é »ªµµ
+}
