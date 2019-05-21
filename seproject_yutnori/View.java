936fb32b9bcf5ecc0bc01b6a3c81e85215fe39ad
Wonhee Cho <42177510+jowoni@users.noreply.github.com>
2019-05-21 19:36:17 +0900

    Add files via upload

--- seproject_yutnori/View.java
+++ seproject_yutnori/View.java
@@ -1,18 +1,21 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class View extends Application implements Observer{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("yut_board.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 932, 647));
         primaryStage.show();
     }
 
@@ -26,7 +29,15 @@ public class View extends Application implements Observer{
         return null;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @FXML
+    private TextField playerNumInput;
+    @FXML
+    private TextField pieceNumInput;
+
+    @FXML
+    private void initialize() {
+        // 연락처 테이블의 두 열을 초기화한다.
+        // firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        // lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
     }
 }
--- seproject_yutnori/YutnoriGame.java
+++ seproject_yutnori/YutnoriGame.java
@@ -1,4 +1,10 @@
 package sample;
 
 public class YutnoriGame {
+    public static void main(String[] args) {
+
+        View view = new View();
+        YutnoriModel yutnoriModel = new YutnoriModel();
+        Controller controller = new Controller(view, yutnoriModel);
+    }
 }
