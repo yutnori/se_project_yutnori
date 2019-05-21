ffa8978d81d7a71e1a50180cdfee1b6e5603d603
Wonhee Cho <michael6487@naver.com>
2019-05-22 02:01:10 +0900

    delete all classes

--- seproject_yutnori/Observer.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public interface Observer {
-    public static final Subject yutnoriSubject = null;
-
-    public void update();
-    public Subject getSubject();
-}
--- seproject_yutnori/Subject.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public interface Subject {
-    public static final Observer observer = null;
-    void registerObserver(Observer init);
-    void notify(YutnoriModel yutnoriModels);
-    Observer getObserver();
-}
